8ae8f79d055fdfd83c01ca4ae406cd511a301887
nworks16 <nworks16@gmail.com>
2024-11-22 18:59:17 -0400

    REPLACE

--- tdact-visitas/tdactvs/visitas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from visitas.models import PosiblesRiesgos,Vivienda,Vehiculo,MiembroFamilia,Visita
-# Register your models here.
-admin.site.register(PosiblesRiesgos)
-admin.site.register(Vivienda)
-admin.site.register(Vehiculo)
-admin.site.register(MiembroFamilia)
-admin.site.register(Visita)
