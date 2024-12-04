107a01019f8ba6c1855262a2c5b46f7a62477c41
JohnFSDev <johnluis22@outlook.com>
2024-12-04 13:30:08 -0400

    Modificacion de static routes en vercel.json

--- audit/staticfiles/core/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from core.models import Usuario
-# Register your models here.
-admin.site.register(Usuario)
--- audit/staticfiles/visitas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Visita
-
-class VisitaForm(forms.ModelForm):
-    class Meta:
-        model = Visita
-        fields = '__all__'  # O puedes especificar los campos que quieres incluir en el formulario manualmente
