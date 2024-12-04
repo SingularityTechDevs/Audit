18a70a08a3fa67d2bc47a505e88c80cd04e4de0d
JohnFSDev <johnluis22@outlook.com>
2024-12-04 13:21:59 -0400

    actualizacion de wsgi.py

--- audit/audit/wsgi.py
+++ audit/audit/wsgi.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'audit.audit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'audit.settings')
 
 application = get_wsgi_application()
 
