c4a60c9e1740eb5f70d1cacea39bb204101c71d5
JohnFSDev <johnluis22@outlook.com>
2024-12-04 12:47:35 -0400

    actualizacion de vercel.json

--- audit/audit/settings.py
+++ audit/audit/settings.py
@@ -5,7 +5,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-skr2!wj4s&bqd85pt=bf#b*d5%7&6d=0!$!i8rn4(^uxp&rbut'
 
-DEBUG = False
+DEBUG = True  # Temporalmente para ver el error
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
 
 ALLOWED_HOSTS = ['178.128.145.204','127.0.0.1','.vercel.app']
 
