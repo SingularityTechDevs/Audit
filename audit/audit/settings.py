7a1ad659f5de112684d983ba391646b299f6cd0d
JohnFSDev <johnluis22@outlook.com>
2024-12-04 12:30:32 -0400

    Desactive el CORS

--- audit/audit/settings.py
+++ audit/audit/settings.py
@@ -9,7 +9,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['178.128.145.204','127.0.0.1','.vercel.app']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 INSTALLED_APPS = [
    'whitenoise.runserver_nostatic',
