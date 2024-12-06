ce17a41e4ac24feee796a4d3383a06bd60da7b57
nworks16 <nworks16@gmail.com>
2024-12-05 22:58:15 -0400

    fix para que vea la base de datos sqlite

--- audit/audit/settings.py
+++ audit/audit/settings.py
@@ -85,7 +85,7 @@ AUTH_USER_MODEL = 'core.Usuario'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    }
 }
 
