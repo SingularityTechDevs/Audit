3ccc8c5578e5adc5bed7fadbe51da04dcc440ccf
nworks16 <nworks16@gmail.com>
2025-04-03 05:51:23 -0400

    FIX ULTIMAS ACTUALIZACIONES

--- audit/audit/settings.py
+++ audit/audit/settings.py
@@ -25,7 +25,7 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = ['178.128.145.204','127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['178.128.145.204','127.0.0.1','.vercel.app','singularisys.com','www.singularisys.com']
 
 
 #Apagar y encender los cors para que los js funcionen haciendo el request al mismo origen
